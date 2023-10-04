0b1b46c6b0390960d8c728ef804c95057f29a26c
Gina Ahumada <Ginaahumadaa@gmail.com>
2023-10-04 14:42:27 -0500

    adding corrections

--- scripts/responsive-menu.js
+++ scripts/responsive-menu.js
@@ -1,13 +1,7 @@
-// Get references to the menu elements
-const menuToggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
+const HamButton = document.querySelector("#menu");
+const navigation = document.querySelector("nav");
 
-// Add a click event listener to the hamburger button
-menuToggle.addEventListener('click', function () {
-  // Toggle the visibility of the menu
-  if (menu.style.display === 'block') {
-    menu.style.display = 'none';
-  } else {
-    menu.style.display = 'block';
-  }
-});
+HamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    HamButton.classList.toggle("open");
+})
