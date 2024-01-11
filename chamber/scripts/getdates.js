3b3b4b65fbe73cc015feecdd088959b9d62f46e9
Gina Ahumada <Ginaahumadaa@gmail.com>
2024-01-10 20:06:48 -0500

    week 1

--- chamber/scripts/getdates.js
+++
@@ -1,6 +0,0 @@
-const currentDate = new Date();
-const currentYear = currentDate.getFullYear();
-document.querySelector('#year').innerHTML += currentYear;
-
-const lastModification = document.querySelector('#last-modification');
-lastModification.textContent += document.lastModified;
