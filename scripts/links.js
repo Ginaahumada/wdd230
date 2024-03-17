32a1ca0a639c68169405814206f92ad8d80aae4a
Gina Ahumada <Ginaahumadaa@gmail.com>
2024-03-17 18:36:39 -0500

    corrections W09

---
+++ scripts/links.js
@@ -0,0 +1,36 @@
+const list = document.querySelector('#list');
+const linksURL = "https://ginaahumada.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data.lessons);
+}
+
+const displayLinks = (weeks) => {
+    weeks.forEach((week) => {
+
+        let listItem = document.createElement('li');
+        
+
+        listItem.textContent = `${week.lesson}: `;
+        
+        week.links.forEach((link, index) => {
+
+            let linkElement = document.createElement('a');
+
+            linkElement.setAttribute('href', link.url);
+            linkElement.setAttribute('target', '_blank');
+            linkElement.textContent = link.title;
+            listItem.appendChild(linkElement);
+            
+            if (index < week.links.length - 1) {
+                listItem.innerHTML += ' | ';
+            }
+        });
+        
+        list.appendChild(listItem);
+    });
+};
+
+getLinks();
